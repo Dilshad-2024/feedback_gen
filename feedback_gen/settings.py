9a1024b27133c58e22206b4d0b107a84c0d84eb9
HEX-995 <mohd.dilshad.ahmed9376@gmail.com>
2026-07-02 11:26:19 +0530

    Prepare project for Render deployment

--- feedback_gen/settings.py
+++ feedback_gen/settings.py
@@ -20,14 +20,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-cek26a4!k$i0-k-&uh058*7gq5*s1g###gm0fx)3=)ws(*0q#w"
+import os
+
+SECRET_KEY = os.environ.get("tASNEEMA@1973")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "ratewise.onrender.com",
+]
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://ratewise.onrender.com",
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -121,9 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
