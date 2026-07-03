1c91811ef102079a5d7adcfc75dd263eaa84f904
HEX-995 <mohd.dilshad.ahmed9376@gmail.com>
2026-07-03 11:39:18 +0530

    Improved Model Loading

--- feedback/sentiment.py
+++ feedback/sentiment.py
@@ -1,15 +1,33 @@
+# from transformers import pipeline
+
+
+# classifier = pipeline(
+#     "sentiment-analysis",
+#     model="cardiffnlp/twitter-roberta-base-sentiment-latest"
+# )
+
+# def analyze_sentiment(text):
+
+#     result = classifier(text)[0]
+
+#     result["label"] = result["label"].upper()
+
+#     return result
 from transformers import pipeline
 
+classifier = None
 
-classifier = pipeline(
-    "sentiment-analysis",
-    model="cardiffnlp/twitter-roberta-base-sentiment-latest"
-)
 
 def analyze_sentiment(text):
+    global classifier
+
+    if classifier is None:
+        classifier = pipeline(
+            "sentiment-analysis",
+            model="cardiffnlp/twitter-roberta-base-sentiment-latest"
+        )
 
     result = classifier(text)[0]
-
     result["label"] = result["label"].upper()
 
     return result
